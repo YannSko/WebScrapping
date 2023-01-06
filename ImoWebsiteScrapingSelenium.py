d1dfe19f03f9b49ff52407bbce72b0c4a2751b6e
YannSko <101813005+YannSko@users.noreply.github.com>
2023-01-06 20:03:36 +0100

    Parse One Time Each Page Product
    I finally find a method to parse only one time :+1:
    
    I stock in a List each driver.title ( page title) ,
    If the Page title already in the list , go on the next page ( i used to try with a string associate to ma page_tabs[i] but idk why it didnt work )
    
    I will try to find an optimize way to check if the page has been already parsed ( because , more there are page in the website , longer will be the time to chech if the title is already in the list )

--- ImoWebsiteScrapingSelenium.py
+++ ImoWebsiteScrapingSelenium.py
@@ -1,6 +1,6 @@
 driver = webdriver.Firefox()
 data1=[]
-
+Checklist=[]
 
 for p in range(0,101):
     driver.get(f'https://www.bienici.com/recherche/achat/paris-75000?page={p}')
@@ -16,28 +16,32 @@ for p in range(0,101):
 
 
         print(len(NbrAppart))
-        #On recupe les liens de chaque offre un par un 
-
+#On recupe les liens de chaque offre un par un 
         LinkAppart = driver.find_elements(By.CLASS_NAME,"detailedSheetLink")
 
 
         page_tabs = [x.get_attribute('href') for x in driver.find_elements(By.CLASS_NAME,"detailedSheetLink")]
         for i in range(len(page_tabs)):
-            if page_tabs[i] != " Checked":
-                driver.get(page_tabs[i])
-                page_tabs[i]= "Checked"
-                time.sleep(1)
-
+    
+            driver.get(page_tabs[i])
+            time.sleep(3)
+            if driver.title not in Checklist :
+                Offers = WebDriverWait(driver, 3).until 
+                EC.presence_of_element_located((By.CLASS_NAME,"detailedSheetContainer"))
                 Offers= driver.find_elements(By.CLASS_NAME,"detailedSheetContainer")
+        
                 for Offer in Offers:
                     Adress = Offer.find_elements(By.CLASS_NAME,"fullAddress")
                     print(Adress[0].text)
                     AdressStock = Adress[0].text
                     data1.append(AdressStock)
                     print('Page navigated after click: ' + driver.title)
+                    Checklist.append(driver.title)
+            
+            
             else:
                 pass
-        #or link in LinkAppart:
-            #or a in range(len(dAdress)):
-        for o in range (len(data1)):
-            print(data1[o])
+#or link in LinkAppart:
+    #or a in range(len(dAdress)):
+for o in range (len(data1)):
+    print(data1[o])
