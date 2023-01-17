309d78eadd3cf6fd8f8182cdc2bcaa6a11a8cfc8
YannSko <yskonieczny@gmail.com>
2023-01-17 16:41:11 +0100

    Update Graphe.py
    training on graph today

--- Graphe.py
+++ Graphe.py
@@ -45,3 +45,30 @@ print(counts)
 ​
 
 Counter({'product'})
+
+import pandas as pd
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+print("Setup Complete")
+
+
+Coffee_Filepath = "coffee_analysis.csv"
+Coffee_Data = pd.read_csv(Coffee_Filepath, index_col="name")
+Coffee_Data
+
+# Largeur et hauteur du graph
+plt.figure(figsize=(10,6))
+
+
+plt.title("Lien entre prix et type du café")
+
+# Bar chart 
+sns.barplot(x=Coffee_Data['roast'], y=Coffee_Data['100g_USD'])
+
+# lab de chaque ax
+plt.ylabel("Prix moyen au 100g")
+plt.xlabel(" Type de café")
+
+
